5e35e5d2fc5ddb7578c1e77085925e0d50dbcc06
Hariprasath96 <hasathphy96@gmail.com>
2023-12-07 23:02:01 +0530

    add few images and created well

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ const App = () => {
 
 <iframe width="1903" height="1070" src="https://www.youtube.com/embed/cWSGs1boCks?list=TLGG0OeiWFwZbBcwNzEyMjAyMw" title="Decibullz Custom Molded Earplugs and Hearing Protection. A fit for every lifestyle." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
-{/* some message about our website */}
+{/* some message about our website from left to right */}
 
 <main className="flex flex-row items-center justify-between w-screen h-auto p-4" >
 <div className="flex flex-col items-center justify-between  h-auto gap-12 p-8" >
@@ -109,10 +109,35 @@ IS BETTER</h1>
   </div>
 </div>
 <div>
-  <img src="./src/images/zig-zag-right.jpg" alt="" />
+  <img src="./src/images/zig-zag-right.jpg" alt="#" />
 </div>
 </main>
 
+
+{/*   some message about our website from right to left*/}
+
+<main className="flex flex-row items-center justify-between w-screen h-auto p-10 relative left-12 " >
+<div>
+  <img src="./src/images/zig-zag-left.jpg" alt="#" />
+</div>
+<div className="flex flex-col items-center justify-between  h-auto gap-12 p-8" >
+  <h1 className="text-6xl font-bold w-[44.3rem] relative right-20  ">THE DECIBULLZ
+EASY SHAPE SYSTEM™</h1>
+  <div className="flex flex-col items-center justify-between w-3/4 relative right-20 h-auto gap-5">
+<h3 className="text-4xl font-bold  text-orange-500 w-3/4 relative right-20 ">HEAT</h3>
+<p className=" text-2xl   text-black pl-4 ">Simply heat the earpieces in warm water for 3-5 minutes. The earpieces will soften for easy moldability.</p>
+  </div>
+  <div className="flex flex-col items-center justify-between w-3/4 relative right-20 h-auto gap-5">
+<h3 className="text-4xl font-bold  text-orange-500  w-3/4 relative right-20"> SHAPE</h3>
+<p  className=" text-2xl   text-black pl-4 ">Place the now pliable earpiece into your ear and form them with the flat of your finger for a snug, secure fit.</p>
+  </div>
+  <div className="flex flex-col items-center justify-between w-3/4 relative right-20 h-auto gap-5">
+<h3 className="text-4xl font-bold  text-orange-500 w-3/4 relative right-20">REMOLDABLE</h3>
+<p  className=" text-2xl   text-black pl-4 ">Decibullz earpieces can be re-warmed and molded to the shape of your ears until you find the perfect fit.</p>
+  </div>
+</div>
+
+</main>
 </>
     
   
