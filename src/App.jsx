60f5806413b4e752d6e277d632cabe1e39d2f410
Hariprasath96 <hasathphy96@gmail.com>
2023-12-07 19:54:25 +0530

    add video on my project

--- src/App.jsx
+++ src/App.jsx
@@ -87,6 +87,10 @@ const App = () => {
  
   </div>
 </main>
+
+{/* add video about Decibullz */}
+<iframe width="1903" height="1070" src="https://www.youtube.com/embed/cWSGs1boCks?list=TLGG0OeiWFwZbBcwNzEyMjAyMw" title="Decibullz Custom Molded Earplugs and Hearing Protection. A fit for every lifestyle." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
 </>
     
   
