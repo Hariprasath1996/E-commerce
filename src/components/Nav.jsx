147f81a0ab59fa811c4bf84557b3940a35414ef0
Hariprasath96 <hasathphy96@gmail.com>
2023-12-08 22:28:13 +0530

    data added well

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,21 +6,21 @@ import { TiShoppingCart } from "react-icons/ti";
 const Navbar = ({item}) => {
     return ( 
         <nav className=' w-screen h-auto flex items-center justify-around p-2 bg-gradient-to-r from-cyan-500 to-blue-500 ... hover:cursor-pointer '  >
-    <div className="logo-container h-25 flex flex-row items-center justify-center hover:  " >
+    <div className="logo-container w-auto h-25 flex flex-row items-center justify-center hover:  " >
     <a  href="https://youtu.be/CIpIQVJkanI?si=liWiGQiMnodJiRpj " target="_blank"><img className="rounded-lg "  src="./src/images/skull-candy.png " alt="#" /></a> 
     </div>
     <div >
-    <ul className="menu-bar w-70 h-25 flex flex-row items-center justify-center gap-8 text-xl  text-white  ">
-    <button href=""><li className="All-Products hover:translate-y-1 p-3 bg-white text-black rounded-md font-bold  ">All-Products</li></button>
-    <a href=""><li className="Wired-Headsets hover:translate-y-1 p-3 bg-white text-black rounded-md font-bold  ">Wired-Headsets</li></a>
-    <a href=""><li className="wireless-HeadPhones hover:translate-y-1 p-3 bg-white text-black rounded-md font-bold  ">wireless-HeadPhones</li></a>
-    <a href=""> <li className="Ear-buds hover:translate-y-1 p-3 bg-white text-black rounded-md font-bold  ">Ear-buds</li></a>
-    <a href=""><li className="Gaming-Headsets hover:translate-y-1 p-3 bg-white text-black rounded-md font-bold  ">Gaming-Headsets</li></a>
-    <a href=""> <li className="Support hover:translate-y-1 p-3 bg-white text-black rounded-md font-bold  ">Support</li>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       </a>
+    <ul className="menu-bar w-70 h-25 flex flex-row items-center justify-center gap-10 text-xl  text-white  ">
+    <button className="" href=""><li className="All-Products hover:translate-y-1 p-2 bg-white text-black rounded-md font-bold border-4 border-indigo-300/100 ... ">ALL-PRODUCTS</li></button>
+    <a href=""><li className="hover:translate-y-1 p-2 bg-white text-black rounded-md font-bold border-4 border-indigo-300/100 ... ">HUNTING </li></a>
+    <a href=""><li className=" hover:translate-y-1 p-2 bg-white text-black rounded-md font-bold border-4 border-indigo-300/100 ... ">INDUSTRIAL </li></a>
+    <a href=""> <li className=" hover:translate-y-1 p-2 bg-white text-black rounded-md font-bold border-4 border-indigo-300/100 ... ">MUSIC</li></a>
+    <a href=""><li className="hover:translate-y-1 p-2 bg-white text-black rounded-md font-bold border-4 border-indigo-300/100 ..."> SECURITY</li></a>
+    <a href=""> <li className=" hover:translate-y-1 p-2 bg-white text-black rounded-md font-bold border-4 border-indigo-300/100 ..."> SAFETY</li>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       </a>
     <a href=""></a>
     </ul>
     </div>
-    <div className=" icons w-15 h-25 flex flex-row items-center justify-center gap-8 text-white" >
+    <div className=" icons w-15 h-25 flex flex-row items-center justify-center gap-6 text-white" >
     <FaSearch className=" text-2xl hover:translate-y-1" />
     <CgProfile className="text-2xl hover:translate-y-1 " />
     <TiShoppingCart className=" text-yellow-400 text-2xl hover:translate-y-1" />
---
+++ src/constants/index.js
@@ -0,0 +1,133 @@
+
+const  allProducts = [
+    {
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:https://cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2
+      
+  }
+  {
+    id: 2,
+    name: "Percussive Shooting Filters",
+    price: 'price: ₹ 2400 ',
+    imgUrl:https://cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/74/787/Percussive---Float__36791.1698770137.jpg?c=2
+      
+  }{
+    id: 3,
+    name: " Earplug Pro-Pack",
+    price: 'price: ₹ 3200 ',
+    imgUrl:https://cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/80/909/Decibullz-Pro-Post-Black_Final__93678.1698770142.jpg?c=2
+      
+  }{
+    id: 4,
+    name: " Earplug Lanyard",
+    price: 'price: ₹ 2200 ',
+    imgUrl:https://cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/77/648/Lanyard_Only__67565.1698770141.png?c=2
+      
+  }{
+    id: 5,
+    name: "Professional High Fidelity Earplugs",
+    price: 'price: ₹ 3800 ',
+    imgUrl:https://cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/86/786/Pro-Hi-Fi-Float__82510.1698770463.jpg?c=2
+      
+  }{
+    id: 6,
+    name: "Fidelity Earplugs",
+    price: 'price: ₹ 3600 ',
+    imgUrl:https://cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/102/783/Hi-Fi-Float__65137.1698770479.jpg?c=2
+      
+  }
+//   
+  {
+    id: 7,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }{
+    id: 1,
+    name: "A Custom Molded Earplugs",
+    price: 'price: ₹ 2800 ',
+    imgUrl:"https//cdn11.bigcommerce.com/s-r69yc54oxa/images/stencil/900x1200/products/75/794/FALLING-NATURAL-PLUGS-black__59340.1698770138.jpg?c=2"
+      
+  }
+]
