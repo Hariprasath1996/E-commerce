b4d598f182d71209a45743a8e3a24db6667c8247
Hariprasath96 <hasathphy96@gmail.com>
2023-12-08 16:31:03 +0530

    some styles changed in nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,18 +10,17 @@ const Navbar = ({item}) => {
     <a  href="https://youtu.be/CIpIQVJkanI?si=liWiGQiMnodJiRpj " target="_blank"><img className="rounded-lg "  src="./src/images/skull-candy.png " alt="#" /></a> 
     </div>
     <div >
-    <a href="">
-    <ul className="menu-bar w-70 h-25 flex flex-row items-center justify-center gap-10 text-xl  text-white  ">
-    <li className="All-Products hover:translate-y-1  ">All-Products</li>
-    <li className="Wired-Headsets hover:translate-y-1 ">Wired-Headsets</li>
-    <li className="wireless-HeadPhones hover:translate-y-1 ">wireless-HeadPhones</li>
-    <li className="Ear-buds hover:translate-y-1 ">Ear-buds</li>
-    <li className="Gaming-Headsets hover:translate-y-1 ">Gaming-Headsets</li>
-    <li className="Support hover:translate-y-1 ">Support</li>
+    <ul className="menu-bar w-70 h-25 flex flex-row items-center justify-center gap-8 text-xl  text-white  ">
+    <button href=""><li className="All-Products hover:translate-y-1 p-3 bg-white text-black rounded-md font-bold  ">All-Products</li></button>
+    <a href=""><li className="Wired-Headsets hover:translate-y-1 p-3 bg-white text-black rounded-md font-bold  ">Wired-Headsets</li></a>
+    <a href=""><li className="wireless-HeadPhones hover:translate-y-1 p-3 bg-white text-black rounded-md font-bold  ">wireless-HeadPhones</li></a>
+    <a href=""> <li className="Ear-buds hover:translate-y-1 p-3 bg-white text-black rounded-md font-bold  ">Ear-buds</li></a>
+    <a href=""><li className="Gaming-Headsets hover:translate-y-1 p-3 bg-white text-black rounded-md font-bold  ">Gaming-Headsets</li></a>
+    <a href=""> <li className="Support hover:translate-y-1 p-3 bg-white text-black rounded-md font-bold  ">Support</li>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       </a>
+    <a href=""></a>
     </ul>
-    </a>
     </div>
-    <div className=" icons w-15 h-25 flex flex-row items-center justify-center gap-10   text-white" >
+    <div className=" icons w-15 h-25 flex flex-row items-center justify-center gap-8 text-white" >
     <FaSearch className=" text-2xl hover:translate-y-1" />
     <CgProfile className="text-2xl hover:translate-y-1 " />
     <TiShoppingCart className=" text-yellow-400 text-2xl hover:translate-y-1" />
