cc50d46153b0ec81b40715dfc7bb6fadc7ed519a
Hariprasath96 <hasathphy96@gmail.com>
2023-12-09 19:55:43 +0530

    use effect hooks method  applied here

--- src/components/Url.jsx
+++ src/components/Url.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { url } from "./constants/dataUrl";
 
 
 // create data's with copied url from api store
 
 const dataUrl= ()=>{
- const [data,setData]=useState([])
+const [products,setProducts]=useState([])
 
+//  create async programs here
 
+const fetchProducts = async ()=>{
+    try{
+        const response =await fetch (url);
+        const data = await response .json ();
+        setProducts(data)
+    }catch (Error){
+        console.log(`something went wrong`,Error)
+    }
+};
 
+// call the fetch function in use effect hook method 
+useEffect(()=>{ 
+    fetchProducts()},[])
 
 }
