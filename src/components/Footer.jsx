cd880e2d83b2b2100644d1fddd32dd24b6a651bb
Hariprasath96 <hasathphy96@gmail.com>
2023-12-09 22:19:21 +0530

    styles changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <main className="bg-gradient-to-r from-cyan-500 to-blue-500 ...w-screen h-auto  ">
 <div className="flex flex-row items-start justify-around p-4 gap-16 leading-2">
 <div className="logo">
-<a  href="https://youtu.be/CIpIQVJkanI?si=liWiGQiMnodJiRpj " target="_blank"><img className="rounded-lg "  src="./src/images/skull-candy.png " alt="#" /></a> 
+<a  href="https://youtu.be/CIpIQVJkanI?si=liWiGQiMnodJiRpj " target="_blank"><img className="rounded-lg  "  src="./src/images/skull-candy.png " alt="#" /></a> 
 </div>
 <ul className="text-white font-bold relative pt-2 bottom-3 text-2xl gap-2">
 <a href=""><li className="p-1">Terms of Service</li> </a>
@@ -16,16 +16,16 @@ const Footer = () => {
 <a href=""><li className="p-1">FAQ</li>
 </a>
 </ul>
-<button className="contact-us relative top-5  bg-gradient-to-r from-white to-white hover:from-pink-500 hover:to-yellow-500 ... rounded p-3 text-xl text-black font-bold  ">CONTACT US</button>
-<button className="subscribe relative top-5 bg-gradient-to-r from-white to-white hover:from-pink-500 hover:to-yellow-500 ... p-3 font-bold text-xl  text-black rounded">SUBSCRIBE</button>
+<button className="contact-us relative top-5  bg-gradient-to-r from-white to-white hover:from-white hover:to-gray-400 ... rounded p-3 text-xl text-black font-bold  ">CONTACT US</button>
+<button className="subscribe relative top-5 bg-gradient-to-r from-white to-white hover:from-white hover:to-gray-400 ... ... p-3 font-bold text-xl  text-black rounded">SUBSCRIBE</button>
 </div>
 
 
-<div className="icon flex flex-row  items-center justify-around p-6   bg-gradient-to-r from-cyan-500 to-blue-500 ...   ">
-<a className=" flex flex-row  items-center justify-around p-4 gap-5" href="">
-    <button className="font-bold  text-2xl  text-blue-600  flex flex-row p-3 rounded  items-center justify-around gap-2 bg-gradient-to-r from-white to-white hover:from-pink-500 hover:to-yellow-500 ..."><BsFacebook />Facebook </button>
-<button className="font-bold  text-2xl text-red-600  flex flex-row p-3 rounded   items-center justify-around gap-2 bg-gradient-to-r from-white to-white hover:from-pink-500 hover:to-yellow-500 ...  "><FaYoutube  />Youtube</button>
-<button className="font-bold  text-2xl text-sky-600  flex flex-row p-3 rounded   items-center justify-around gap-2 bg-gradient-to-r from-white to-white hover:from-pink-500 hover:to-yellow-500 ..."><BsTwitterX />Twitter</button>
+<div className=" w-screen h-auto icon flex flex-row  items-center justify-around p-6   bg-gradient-to-r from-cyan-500 to-blue-500 ...   ">
+<a className=" w-screen h-auto  flex flex-row  items-center justify-around   relative left- " href="">
+<button className="font-bold  text-2xl  text-blue-600  flex flex-row p-3 rounded  items-center justify-around gap-2 bg-gradient-to-r from-white to-white hover:from-white hover:to-blue-300 ..."><BsFacebook />Facebook </button>
+<button className="font-bold  text-2xl text-red-600  flex flex-row p-3 rounded   items-center justify-around gap-2 bg-gradient-to-r from-white to-white hover:from-white hover:to-red-400 ...  "><FaYoutube  />Youtube</button>
+<button className="font-bold  text-2xl text-sky-600  flex flex-row p-3 rounded   items-center justify-around gap-2 bg-gradient-to-r from-white to-white hover:from-white hover:to-sky-300 ..."><BsTwitterX />Twitter</button>
 </a>
 
 </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ import { TiShoppingCart } from "react-icons/ti";
 
 const Navbar = ({item}) => {
     return ( 
-        <nav className=' w-screen h-auto flex items-center justify-around p-2 bg-gradient-to-r from-cyan-500 to-blue-500 ... hover:cursor-pointer '  >
+        <nav className=' w-screen h-auto flex items-center justify-around p-2 bg-gradient-to-r from-cyan-500 to-blue-500 ...'  >
     <div className="logo-container w-auto h-25 flex flex-row items-center justify-center hover:  " >
     <a  href="https://youtu.be/CIpIQVJkanI?si=liWiGQiMnodJiRpj " target="_blank"><img className="rounded-lg "  src="./src/images/skull-candy.png " alt="#" /></a> 
     </div>
