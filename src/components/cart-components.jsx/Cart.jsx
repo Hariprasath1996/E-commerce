faabf640376adc436d8f0cb682c186d7348a5cc9
Hariprasath96 <hasathphy96@gmail.com>
2024-01-20 20:24:24 +0530

    cart created

---
+++ src/components/cart-components.jsx/Cart.jsx
@@ -0,0 +1,14 @@
+
+const CartItem = ({name,quantity,image,price})=>{
+return(
+    <main className="cart-items-container">
+        <img src={image}alt={name} />
+        <div>
+            <span className="name">{name}</span>
+            <span className="price">
+                {quantity} X ₹{price}
+            </span>
+        </div>
+    </main>
+)
+}
---
+++ src/components/routes/context/cart.context.jsx
@@ -0,0 +1,44 @@
+import { Children } from "react";
+import { createContext,useContext,useEffect,useState } from "react";
+const CartContext =createContext();
+
+const value={}
+
+const addCartItem =(cartItems,ProductToAdd)=>{
+
+const existingCartItem=cartItems.find(
+    (cartItem)=>cartItem.id === ProductToAdd.id
+);
+if (existingCartItem) {
+    return cartItems.map((cartItem)=>
+        cartItem.id === ProductToAdd.id
+        ? {...cartItem,quantity:cartItem.quantity+1}
+        : cartItem
+
+    
+    )
+}
+
+
+return [...cartItems,{...ProductToAdd,quantity:1}];
+}
+
+
+const CartProvider = ()=>{
+const [isCartOpen,setIsCartOpen]=useState(false);
+const [cartItems,setCartItems]=useState([]);
+const [cartCount,setCartCount]=(0);
+const [cartTotal,setCartTotal]=(0)
+
+
+
+
+    return <CartContext.Provider value={value}>
+        {Children}
+    </CartContext.Provider>
+}
+const useCartGlobalContext =()=>{
+    return useContext(CartContext)
+};
+
+export {CartProvider,useCartGlobalContext}
